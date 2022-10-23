f1fca9f95b66b4e35f444627282aba24a511daf4
shafa81 <114590314+shafa81@users.noreply.github.com>
2022-10-23 21:53:25 +0700

    tugas sesi 5

---
+++ sesi5/kasir.py
@@ -0,0 +1,25 @@
+print("warung")
+print("pilih menu")
+print("1. susu putih -> 5.000 ")
+print("2. kopi hitam -> 4.000")
+print("roti bakar -> 10.000")
+
+pilih = int(input("masukan menu pilihan: "))
+if pilih == 1:
+    print("kopi hitam dengan harga Rp.4.000")
+    jumlah = int(input("jumlah pesanan")) 
+    print("")
+    jumlah==4.000 
+    print("=====TERIMAKASI=====")
+elif pilih == 2:
+    print("susu putih dengan harga Rp. 5.000")
+    jumlah = int(input("jumlah pesanan")) 
+    print("")
+    jumlah==5.000
+    print("=====TERIMAKASI=====")
+elif pilih == 3:
+    print("roti bakar dengan harga Rp. 10.000")
+    jumlah = int(input("jumlah pesanan")) 
+    print("")
+    jumlah==10.000
+    print("=====TERIMAKASI=====")
